95a1aaca48a026c037527d952f0e7f8a2758f488
Mateusz Wolsza <mateusz.wolsza@enova.pl>
2013-06-20 16:27:38 +0200

    Beginning of analyze

---
+++ DocXToMarkdown.Tests/DocumentAnalyzerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Novacode;
+using Xunit;
+
+namespace DocXToMarkdown.Tests {
+  public class DocumentAnalyzerTests {
+
+    [Fact]
+    public void DocumentAnalyzer_ShouldAnalyzeDocument() {
+      using( var doc = DocX.Load( "./docx/headers_and_lists.docx" ) ) {
+        var analyzer = new DocumentAnalyzer( doc );
+        analyzer.Analyze();
+        var result = analyzer.Result;
+        Assert.Equal( 5, result.Count() );
+        Assert.True( result.ElementAt( 3 ).StartsWith( "ordered_" ) );
+        Assert.True( result.ElementAt( 4 ).StartsWith( "unordered_" ) );
+      }
+    }
+ 
+  }
+}
---
+++ DocXToMarkdown/DocumentAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Novacode;
+
+namespace DocXToMarkdown {
+
+  public class DocumentAnalyzer {
+
+    public DocumentAnalyzer( DocX document ) {
+      _document = document;
+    }
+
+    public void Analyze() {
+      _result = _document.Paragraphs.Select( p => getStyleName( p ) ).Distinct();
+    }
+
+    private string getStyleName( Paragraph p ) {
+      if( p.IsListItem ) {
+        return ( p.ListItemType == ListItemType.Numbered ? "ordered_" : "unordered_" ) + p.StyleName;
+      } else return p.StyleName;
+    }
+
+    public IEnumerable<String> Result {
+      get {
+        return _result;
+      }
+    }
+
+    private IEnumerable<String> _result;
+    private DocX _document; 
+  }
+
+}
